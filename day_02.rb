442530f6e687a912ca017f6128769661f31b5feb
EMCain <>
2017-12-02 16:10:19 -0800

    day 2 part 2

--- day_02.rb
+++ day_02.rb
@@ -11,5 +11,31 @@ def find_checksum(filename)
   return sum
 end
 
-puts("sum for test: " + find_checksum('./test_inputs/day_02_01.txt').to_s)
-puts("sum for input: " + find_checksum('./inputs/day_02.txt').to_s)
+def find_divisible(filename)
+  sum = 0
+  IO.foreach(filename) do | line |
+    nums = line.strip.split(' ')
+    nums = nums.map { |e| e.to_i }
+
+    quotient = 0
+    while quotient == 0 do
+      for numerator in nums
+        for denominator in nums
+          if numerator != denominator
+            if numerator % denominator == 0
+              quotient = numerator / denominator
+              break
+            end
+          end
+        end
+      end
+    end
+    sum += quotient
+  end
+  return sum
+end
+puts "sum for test: " + find_checksum('./test_inputs/day_02_01.txt').to_s
+puts "sum for input: " + find_checksum('./inputs/day_02.txt').to_s
+
+puts "divisible for test: " + find_divisible('./test_inputs/day_02_02.txt').to_s
+puts "divisible for input: " + find_divisible('./inputs/day_02.txt').to_s
