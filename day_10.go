7375bc96bf8956d347a381bd27312dae2611f8d2
EMCain <>
2017-12-10 15:06:21 -0800

    added hash function basics, still need to reverse sublists

--- day_10.go
+++ day_10.go
@@ -14,10 +14,31 @@ func makeRing(size int) *ring.Ring {
   return r
 }
 
+func hash(r *ring.Ring, input []int) *ring.Ring {
+  skip := 0
+  newRing := ring.New(r.Len())
+  for i := 0; i < len(input); i++ {
+    sublist_size := input[i]
+    sublist := make([]int, sublist_size)
+    for j := 0; j < sublist_size; j++ {
+      sublist[j] = r.Value.(int)
+      r = r.Next()
+    }
+    fmt.Printf("%v", sublist)
+    // TODO reverse list; should I apply this to r rather than creating newRing?
+    for j:= 0; j < skip; j++ {
+      r = r.Next()
+    }
+    skip++
+  }
+  return newRing
+}
+
 func main() {
   smallR := makeRing(5)
 
   smallR.Do(func (x interface{}) {
     fmt.Println(x)
   })
+  hash(smallR, []int {3, 4, 1, 5})
 }
